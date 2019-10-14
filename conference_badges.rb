063f4e93d8fb2451af5043863fcf115f6f3926bf
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-14 20:20:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
     return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
   badge_maker.map do |name|
     badge_maker(name)
   end
